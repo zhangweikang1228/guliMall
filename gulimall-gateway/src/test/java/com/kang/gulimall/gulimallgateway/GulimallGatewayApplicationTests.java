986191e42a9a390cf3bf0227a4eacc8532b6f3d3
zhangweikang1228 <565927608@qq.com>
2024-06-29 22:21:12 +0800

    gateway 配置成功

---
+++ gulimall-gateway/src/test/java/com/kang/gulimall/gulimallgateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,20 @@
+package com.kang.gulimall.gulimallgateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 1、开启服务注册发现
+ *  (配置nacos的注册中心地址)
+ * 2、编写网关配置文件
+ */
+
+@SpringBootTest
+public class GulimallGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
